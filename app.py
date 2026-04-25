5dd326013be936d7a2979d7271c1ee6b7f3839cb
sebotas07 <sebastianbarahona507@gmail.com>
2026-04-25 13:30:36 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mi App", page_icon="👋")
+
+st.title("Hola 👋")
+st.subheader("Bienvenido a tu nueva App")
+st.write("Esta es una prueba básica de Streamlit.")
